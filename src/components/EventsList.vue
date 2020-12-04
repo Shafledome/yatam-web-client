d3b7dc33aeb6f08353849a8dde754902a45f1d2e
fran <fran98bg@gmail.com>
2020-12-04 19:53:29 +0100

    first version of Events

---
+++ src/components/EventsList.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="event-list">
+        <div class="column">
+            <EventPreview v-for="event in events"
+            v-bind:key="event.ID_ACTIVIDAD"
+            v-bind:name="event.NOMBRE"
+            v-bind:event="event.EVENTO"
+            v-bind:description="event.DESCRIPCION"
+            v-bind:email="event.E_MAIL"
+            v-bind:initDate="event.F_INICIO"
+            v-bind:endDate="event.F_FIN"
+            v-bind:url="event.DIRECCION_WEB"
+            v-bind:type="event.CATEGORIA"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import EventPreview from './EventPreview.vue'
+
+export default {
+    name: "EventList",
+    components: {
+        EventPreview
+    },
+    data() {
+        return {
+            events: null,
+            selectedType: '',
+        }
+    },
+    mounted() {
+        axios
+        .get('http://127.0.0.1:30006/events/all')
+        .then(response => (this.events = response.data))
+    },
+    methods: {
+        filteredProducts() {
+
+                if(this.selectedType === '') {
+                    return this.events;
+                } else {
+                    const category = this.selectedType;
+                    return this.Products
+                               .filter((event) => event.CATEGORIA === category)
+                }
+            }
+    }
+}
+</script>
+
+<style>
+    html {
+        height: 100%;
+        padding-top: 10px;
+        background: #e6ecf1;
+    }
+
+    .leisures-list {
+        margin: auto;
+        max-width: 900px;
+    }
+</style>
