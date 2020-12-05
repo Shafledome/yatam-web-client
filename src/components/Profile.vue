97ad0a3fffed86f9a1e5dbff0c0c93e1f93cf2cf
gonzalo1398 <gonzalo1398@gmail.com>
2020-12-05 03:35:02 +0100

    Profile 2.0

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -4,12 +4,16 @@
         <h1 class="title is-1">{{ values.username }}</h1>
     </div>    
     <div class="column">
+        <h2 class="title is-4">Trophies</h2>
         <TrophyList v-bind:user="key"></TrophyList> 
         <!-- <TrophyList v-for="user in users"
                       v-bind:key="user.id"
                       v-bind:username="user.username"
         ></TrophyList>
         -->
+        <h3 class="title is-4">Reviews</h3>
+        <RatingsList v-bind:data="key"
+                     v-bind:user=true></RatingsList>
     </div>
   </div>
 </template>
@@ -17,11 +21,13 @@
 <script>
 import axios from "axios";
 import TrophyList from './TrophyList'
+import RatingsList from './RatingsList'
 
 export default {
   name: "Profile",
   components: {
-    TrophyList
+    TrophyList,
+    RatingsList
   },
   data() {
     return {
@@ -34,12 +40,12 @@ export default {
   },
   mounted() {
     axios
-    .get('http://127.0.0.1:30006/users/email/mariano@gmail.com')
+    .get('http://127.0.0.1:30006/users/email/juanito@gmail.com')
     .then(response => (this.user = response.data, 
                        this.key = Object.keys(this.user)[0],
                        this.values = Object.values(this.user)[0]))
 
-    this.key = Object.keys(this.user)[0]
+
     /*
     axios
     .get('http://127.0.0.1:30006/users/user_key/-MN0ZWcLFHLjO3Tudsr4')
--- src/components/RatingPreview.vue
+++ src/components/RatingPreview.vue
@@ -5,7 +5,7 @@
         <div class="media-content">
           <div class="content">
             <p>
-              <strong>{{ user }}</strong> <small>{{leisure}}</small>
+              <strong>{{ title }}</strong> <!--<small>{{leisure}}</small>-->
               <br>
               <br>
               <b-rate v-model="grade" icon-pack="fas" disabled="true"></b-rate>
@@ -19,6 +19,8 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   name: "RatingPreview",
   props: {
@@ -26,6 +28,24 @@ export default {
       leisure: String,
       grade: Number,
       text: String,
+      bool: Boolean
+  },
+  data() {
+      return {
+        title : null
+      }
+  },
+  mounted() {
+    if(this.bool == true){
+      axios
+      .get('http://127.0.0.1:30006/users/user_key/' + this.user)
+      .then(response => (this.title = response.data.username))
+    }else{
+      axios
+      .get('http://127.0.0.1:30006/leisures/id/' + this.leisure)
+      .then(response => (this.title = response.data.name))
+    }
+    
   }
 };
 </script>
