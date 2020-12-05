044c5ad0024ff859dadc4829a877f04e2c5d307a
gonzalo1398 <gonzalo1398@gmail.com>
2020-12-05 20:16:29 +0100

    Profiles updated.

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="profile">
-    <div class="user-data" style="text-align:center; background: #0a1b15;">
-        <h1 class="title is-1" style="color:#04c970;  padding: 70px 0;">{{ values.username }}</h1>
+    <div class="user-data" style="text-align:center; background: #ffffff;">
+        <h1 class="title is-1" style="color:#0a1b15;  padding: 70px 0;">{{ values.username }}</h1>
     </div>
-    <div class="columns" style="background:#04c970;">
+    <div class="columns" style="background:#0a1b15;">
         <div class="column is-one-fifth"></div>
         <div class="column is-one-fifth">
-            <p class="title is-4">Trophies</p>
+            <br>
+            <p class="title is-4" style="color:#04c970;">Trophies</p>
             <TrophyList v-bind:user="key"></TrophyList> 
         </div>
         <div class="column">
-            <h3 class="title is-4">Reviews</h3>
+            <br>
+            <h3 class="title is-4" style="color:#04c970">Reviews</h3>
             <RatingsList v-bind:data="key"
                         v-bind:user=true></RatingsList>
         </div>
